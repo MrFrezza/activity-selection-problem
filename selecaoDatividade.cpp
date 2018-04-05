1780c8730eee08c9dad2584d6379dc97f681a553
MrFrezza <frezzans@gmail.com>
2018-04-05 15:13:02 -0300

    Comentarios e formatacao

--- selecaoDatividade.cpp
+++ selecaoDatividade.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// define a estrutura de uma atividade
 struct Atividade {
 	int inicio;
 	int fim;
@@ -22,44 +23,44 @@ struct compara {
 	}
 };
 
-void printaArr(int * arr, int size) {
-	for (int i = 0; i < size; i++)
-	{
-		cout << arr[i] << "\n";
-	}
-}
 
-
+// Ordena os eventos passados pelo final em ordem crescente
 void criaEventos(int s[], int f[], int size) 
 {
+	// Inicia a fila de prioridade
 	priority_queue<Atividade*, vector<Atividade*>, compara> minHeap;
 
+	// Popula a heap
 	for (int i = 0; i < size; i++)
 	{
 		minHeap.push(new Atividade(s[i], f[i]));
 	}
 
-	//printaArr(s, size);
-	//cout << "--------------" << "\n";
-	//printaArr(f, size);
+	int ctrlFim = 0;
 
-	int st = 0, ctrlFim = 0;
+	cout << "Maior sequencia de atividades e seus respectivos tempos de inicializacao e finalizacao: \n\n";
 
+	// Itera pela fila de prioridade exibindo apenas a melhor escolha local
 	for (int i = 0; i < size; i++)
 	{	
+		// Verifica qual é a melhor atividade em sequencia
 		if (ctrlFim <= minHeap.top()->inicio)
 		{
-			cout << i+1 << " = " << minHeap.top()->inicio << " | " << minHeap.top()->fim << "\n";
+			cout << "Atividade " << i+1 << " = Inicio " << minHeap.top()->inicio << " | Fim " << minHeap.top()->fim << "\n";
 			ctrlFim = minHeap.top()->fim;
 		}
 		minHeap.pop();
 	}
+	cout << "\n";
 }
 
 int main ()
 {
+	// Horários de inicio das atividades
 	int s[] = {12, 2, 8, 8, 6, 5, 3, 5, 0, 3, 1};
-	int f[] = { 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4 };
+
+	// Horários final das atividades
+	int f[] = {14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4 };
 
 	criaEventos(s, f, 11);
 
